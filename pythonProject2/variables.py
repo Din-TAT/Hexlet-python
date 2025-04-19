46bbf4bae7d761380c892aff7b9ccf0e6eaac148
DINAR MAKHMUTSHIN <mahmutsindinar@gmail.com>
2025-04-19 12:40:21 +0300

    add variables

---
+++ pythonProject2/variables.py
@@ -0,0 +1,11 @@
+#Найдите в программе необъявленную переменную и объявите ее, присвоив ей значение Dragon
+
+family = 'Targaryen'
+pet = "Dragon"
+# BEGIN (write your solution here)
+
+# END
+
+print(family)
+print(' and ')
+print(pet)
