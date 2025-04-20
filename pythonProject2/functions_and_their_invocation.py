bc5219e761289b8fa266a2ba180923ced001c5cb
DINAR MAKHMUTSHIN <mahmutsindinar@gmail.com>
2025-04-20 16:16:54 +0300

    functions_and_their_invocation.py

---
+++ pythonProject2/functions_and_their_invocation.py
@@ -0,0 +1,12 @@
+#В коде программы определены две переменные, содержащие имена компаний. Посчитайте их общую длину в символах и выведите ее на экран.
+
+#Подсказки
+#Для определения длины строки используйте функцию len().
+
+company_one = "Apple"
+company_two = "Samsung"
+
+len_company_one = len(company_one)
+len_company_two = len(company_two)
+amount = len_company_one + len_company_two
+print(amount)
