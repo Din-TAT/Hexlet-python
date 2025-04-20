d7a2ed833d56a764c374d412d5cd801c015cd4d6
DINAR MAKHMUTSHIN <mahmutsindinar@gmail.com>
2025-04-20 15:52:13 +0300

    add data_types.py

---
+++ pythonProject2/data_types.py
@@ -0,0 +1,7 @@
+#Выведите на экран строку 2 times, полученную из числа 2 и строки times, используя преобразования типов и конкатенацию:
+
+#2 times
+
+one_lines = 2
+two_lines = 'times'
+print(str(one_lines), two_lines)
