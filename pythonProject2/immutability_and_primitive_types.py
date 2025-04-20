67a0600487832c9c1d45169e3694165d0b2ef28d
DINAR MAKHMUTSHIN <mahmutsindinar@gmail.com>
2025-04-20 16:02:02 +0300

    immutability_and_primitive_types.py

---
+++ pythonProject2/immutability_and_primitive_types.py
@@ -0,0 +1,6 @@
+#В переменной value хранится строковое представление числа. Получите модуль этого числа, используя функцию abs(). Запишите его в ту же переменную value. Перед этим нужно привести строку к числу.
+
+value = '-42'
+transform_value = int(value)
+tranform_value_with_sign_plus = abs(transform_value)
+print(tranform_value_with_sign_plus)
