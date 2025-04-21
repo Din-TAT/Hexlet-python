a94ef532debe9a9f7f276093e0d2848db4eb461c
DINAR MAKHMUTSHIN <mahmutsindinar@gmail.com>
2025-04-21 21:32:04 +0300

    returning_values.py

---
+++ pythonProject2/returning_values.py
@@ -0,0 +1,12 @@
+#Определите функцию say_hurray_three_times(), которая возвращает строку hurray! hurray! hurray!
+
+# Вызов функции
+#result = say_hurray_three_times()
+#print(result)  # => hurray! hurray! hurray!
+#Вам нужно только определить функцию, вызывать ее не нужно. Вызов функции будет происходить в тестах при проверке решения
+
+def say_hurray_three_times():
+    result = 'hurray! hurray! hurray!'
+    print(result)
+    return result
+say_hurray_three_times()
