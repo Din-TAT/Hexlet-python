c2d4f1c26ca55283968c049c05df3bbd4f9374e6
DINAR MAKHMUTSHIN <mahmutsindinar@gmail.com>
2025-04-21 13:13:55 +0300

    add function_signature.py

---
+++ pythonProject2/function_signature.py
@@ -0,0 +1,13 @@
+#В Python есть функция abs(). Изучите ее сигнатуру на странице документации.
+#from functions_and_their_invocation import amount
+
+#Напишите программу, которая использует функцию abs() и выводит на экран модуль суммы двух переменных — num1 и num2.
+
+num1 = 10
+num2 = -13
+
+amount_num1_and_num2 = num1 + num2
+abs_amount_num1_and_num2 = abs(amount_num1_and_num2)
+print(abs_amount_num1_and_num2)
+
+
